adeeeaa90e16d2bd5f4f919f5c0ed7e545230a06
Adam Games Studio <106443261+AdamGamesStudio@users.noreply.github.com>
2022-10-10 19:37:37 +0200

    Upload project

---
+++ .TicTacToeGame/app/src/build/generated/source/buildConfig/debug/com/adamgamesstudio/tictactoegame/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.adamgamesstudio.tictactoegame;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.adamgamesstudio.tictactoegame";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "v1.6 COMPLETE STABLE RELEASE";
+}
